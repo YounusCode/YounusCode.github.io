d0a6b8d73d22bd075d7c90c36b7151c589bc7983
Younus Ali <younus_ali45@icloud.com>
2024-06-30 13:38:46 -0500

    new website

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    });
+});
+
+function openSection(event, sectionName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(sectionName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+});
